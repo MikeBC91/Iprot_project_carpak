50ec86d83baa4e3721771178a30ecaa021bfd477
MICHAEL BRICENO CUERVO <BRICEM@tdm.local>
2024-06-21 12:51:59 +0800

    Added constructors and attributes to the display and sensor classes

--- src/car_park.py
+++ src/car_park.py
@@ -3,13 +3,13 @@ class CarPark:
     def __init__(self,
                  location,
                  capacity,
-                 plates,
-                 sensors,
-                 display):
+                 plates = None,
+                 sensors = None,
+                 display = None):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.display = display or []
-        def __str__(self):
-            return f'Welcome to {self.location}car park'
+    def __str__(self):
+        return f'Welcome to {self.location} car park'
--- src/display.py
+++ src/display.py
@@ -1,2 +1,15 @@
 class display:
-    pass
+
+    def __init__(self,
+                 id,
+                 car_park,
+                 message = "",
+                 in_on = False,
+                 ):
+        self.id = id
+        self.message = message
+        self.in_on = in_on
+        self.car_park = car_park
+    def __str__(self):
+        return f'{self.id}: Display is {"is on" if self.is_on else "is off"}'
+
