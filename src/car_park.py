9530c6836f5266f0e9803c1c23a0e2781afd1057
MICHAEL BRICENO CUERVO <BRICEM@tdm.local>
2024-06-21 12:02:53 +0800

    Added a contructor and attributes to the car park

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,15 @@
 class CarPark:
-    pass
+
+    def __init__(self,
+                 location,
+                 capacity,
+                 plates,
+                 sensors,
+                 display):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.display = display or []
+        def __str__(self):
+            return f'Welcome to {self.location}car park'
