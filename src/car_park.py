c9f1b909559f313267d5d535ce636daf8e2bda8d
MICHAEL BRICENO CUERVO <BRICEM@tdm.local>
2024-06-21 17:44:08 +0800

    save

--- src/car_park.py
+++ src/car_park.py
@@ -1,10 +1,16 @@
+import json
+
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime
+import json
 class CarPark:
 
     def __init__(self,
                  location,
                  capacity,
+                 log_file='log.txt',
                  plates = None,
                  sensors = None,
                  displays = None):
@@ -13,6 +19,28 @@ class CarPark:
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+
+        self.log_file = Path(log_file)
+        if not self.log_file.exists():
+            self.log_file.touch()
+
+    def to_json(self, file_name):
+        with open(file_name, 'w') as file:
+            json.dump({"location": self.location,
+                       "capacity": self.capacity,
+                       "log_file": str(self.log_file)}, file)
+
+    @staticmethod
+    def from_json(file_name):
+        """Allows the creation of an instance of CarPark from a JSON.
+        >>> car_park = CarPark.from_json("some_file.txt")"""
+        with open(file_name, 'r') as file:
+            conf = json.load(file)
+        return CarPark(location=conf["location"],
+                       capacity=(conf["capacity"]),
+                       log_file=conf["log_file"])
+
+
 
 
     @property
@@ -30,13 +58,19 @@ class CarPark:
         if isinstance(component, Sensor):
             self.sensors.append(component)
         elif isinstance(component, Display):
-            self.display.append(component)
+            self.displays.append(component)
+
+    def _log_car(self, action, plate):
+        with self.log_file.open(mode='a') as file:
+            file.write(f'{plate} {action} on the {datetime.now().strftime("%d/%m/%Y %H:%M")}\n')
 
     def add_car(self, plate):
         self.plates.append(plate)
+        self._log_car('entered', plate)
 
     def remove_car(self, plate):
         self.plates.remove(plate)
+        self._log_car('exited', plate)
 
     def update_displays(self):
         for display in self.displays:
